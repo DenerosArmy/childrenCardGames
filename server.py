3ce7017d6d1e8450c5c2634d0a35004e639a0123
Vaishaal Shankar <vaishaal@gmail.com>
2013-04-07 01:38:29 -0700

    server flow

--- server.py
+++ server.py
@@ -3,20 +3,21 @@ import tornado.web
 import tornado.websocket 
 import tornado.httpserver
 import yugioh
+D = True
+op = None
 class WSHandler(tornado.websocket.WebSocketHandler):
     def open(self):
-        self.write_message("Hello World")
-
+        op = self
+        if D:
+            print "Opened Socket"
     def on_message(self,message):
-        print 'message received %s' % message 
+        if D: 
+            print 'message received %s' % message 
 
-    def on_close(self):
-        print 'connection closed'
+    
 class RfidHandler(tornado.web.RequestHandler):
-  def get(self,**kwargs):
-      print map(str,self.get_arguments("penis"))[0]
   def post(self):
-    self.write("Hello, chatter! [POST]")
+      print map(str,self.get_arguments("rfid"))
 
 
 application = tornado.web.Application([
@@ -25,6 +26,9 @@ application = tornado.web.Application([
 ])
 
 if __name__ == "__main__":
+    card = yugioh.Card("50003E4082AC","Dark Magician",0) 
+    cards = {card.rfid:card}
+    game = Game(cards,self.op)
     http_server = tornado.httpserver.HTTPServer(application) 
     http_server.listen(8000)
     tornado.ioloop.IOLoop.instance().start() 
--- yugioh.py
+++ yugioh.py
@@ -1,3 +1,4 @@
+import json 
 D = True
 class Card(object):
     def __init__(self,rfid_id, card_name,card_type):
@@ -6,29 +7,18 @@ class Card(object):
         self.card_type = card_type 
         self.position = None 
         self.state = "deck" 
-    def __str__(self):
-        return (self.name,self.position,self.state)
+    def serialize(self):
+        if not self:
+            return
+        return ({"name":self.name,"state":self.state})
 
-class Field(object):
-    def __init__(self):
-        self.monsters = [] 
-        self.spraps = [] 
-        self.graveyard = [] 
-        self.field_card = None 
-
-    def update(self,field): 
-        self.field = field[0] 
-        self.monsters = field[1:6]  
-        self.graveyard = field[6] 
-        self.spraps = field[8:12]
-
+    
 class Game(object):
     def __init__(self,cards,function=str):
         self.cards = {} 
-        self.field = Field()
+        self.field = [None for _ in range(14)]
         self.in_hand = None
         self.function = function
-        self.mat 
     def pick_up(self,id): 
         if D:
             print "Picked up " + self.cards[id] 
@@ -37,5 +27,7 @@ class Game(object):
         if self.in_hand:
             if D:
                 print "Placing card to " + str(location) 
-            #self.function(str(self.in_hand),location,position)
-            
+            self.field[position] = self.in_hand
+        self.function(map(Card.serialize,self.field))
+
+ 
