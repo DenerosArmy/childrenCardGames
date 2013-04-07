5e5a66eefa0dc3ed7e887e9fca145d5a6dd8b46b
Nikita Kitaev <nikitakit@gmail.com>
2013-04-07 10:43:58 -0700

    Expand game logic to allow activating/removing cards

--- yugioh.py
+++ yugioh.py
@@ -1,6 +1,8 @@
 import json 
 D = True
 class Card(object):
+    TYPE_MONSTER = 0
+    TYPE_SPELL = 2
     def __init__(self,rfid_id, card_name,card_type,attack=0,defense=0):
         self.rfid = rfid_id
         self.name = card_name 
@@ -34,21 +36,56 @@ class Game(object):
 	 
      
     def place(self,location,state):
-        if self.in_hand and int(location) < 13:
-	    valid = False
-	    if self.in_hand.card_type == 0: 
-		valid = location in [i for i in range(1,7)] 
-	    else:
-		valid = location in [i for i in range(9,13)] 
-            if not valid:
-		return False
-            if D:
-                print "Placing card " + str(self.in_hand) + " to  "+ str(location) 
-	    
-            print self.field	
+        if not self.in_hand or self.field[location]:
+            return False
 
-            self.field[location] = self.in_hand	
-            self.in_hand.state = state
-	    self.in_hand = None
-	    return True
-    	return False 
+        if self.in_hand.card_type == Card.TYPE_MONSTER:
+            valid_locations = range(1,7)
+        else:
+            valid_locations = range(9,13)
+
+        if location not in valid_locations:
+            return False
+
+        if D:
+            print "Placing card " + str(self.in_hand) + " to  "+ str(location)
+
+        self.field[location] = self.in_hand
+        self.in_hand.state = state
+        self.in_hand = None
+        return True
+
+    def can_remove(self, card):
+        return False
+
+    def remove(self, location, old_state):
+        if self.field[location] is None or self.field[location].state != old_state:
+            return False
+
+        if self.in_hand != self.field[location]:
+            # TODO: this assumes RFID scans before CV
+            return False
+
+        if not self.can_remove(self.in_hand):
+            return False
+
+        self.field[location] = None
+        if self.in_hand.card_type == Card.TYPE_MONSTER:
+            if D:
+                print "monster removed from field"
+        else:
+            if D:
+                print "magic card removed from field"
+        self.in_hand = None
+        return True
+
+    def activate(self, location):
+        if self.field[location] is None or self.field[location].state != "down":
+            return False
+
+        # TODO: effects of activating the card
+        if D:
+            print "card activated:", self.field[location]
+
+        self.field[location].state = "att"
+        return True
